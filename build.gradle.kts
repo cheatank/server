66c00be208d8e7d0fa5d90df785605b80e52ddad
sya-ri <sya79lua@gmail.com>
2022-01-23 20:56:02 +0900

    chore: ktor のバージョンを 1.6.7 に変更

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,10 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.github.cheatank:common:1.0.0-SNAPSHOT")
-    implementation("io.ktor:ktor-server-core:1.6.6")
-    implementation("io.ktor:ktor-server-cio:1.6.6")
-    implementation("io.ktor:ktor-websockets:1.6.6")
+    implementation("io.ktor:ktor-server-core:1.6.7")
+    implementation("io.ktor:ktor-server-cio:1.6.7")
+    implementation("io.ktor:ktor-websockets:1.6.7")
     implementation("org.slf4j:slf4j-simple:1.7.33")
     testImplementation(kotlin("test"))
-    testImplementation("io.ktor:ktor-server-test-host:1.6.6")
+    testImplementation("io.ktor:ktor-server-test-host:1.6.7")
 }
