8bf3903c493a9319df0a6cff352ebe2d98640a77
sya-ri <sya79lua@gmail.com>
2022-01-25 22:11:45 +0900

    feat: UpdateSelfLocation が送信された時に UpdateLocation を他プレイヤーに送信するようにした

--- src/main/kotlin/com/github/cheatank/server/entity/Game.kt
+++ src/main/kotlin/com/github/cheatank/server/entity/Game.kt
@@ -3,11 +3,18 @@ package com.github.cheatank.server.entity
 import com.github.cheatank.common.PacketType
 import com.github.cheatank.common.data.GameData
 import com.github.cheatank.common.data.LocationData
+import com.github.cheatank.common.data.SelfLocationData
 import com.github.cheatank.common.data.ShortData
 import com.github.cheatank.server.utils.close
+import com.github.cheatank.server.utils.readPacket
 import com.github.cheatank.server.utils.sendPacket
+import io.ktor.http.cio.websocket.Frame
 import io.ktor.websocket.DefaultWebSocketServerSession
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
@@ -17,14 +24,12 @@ data class Game(
     private val sessions: List<DefaultWebSocketServerSession>,
 ) {
     private val sessionById: Map<Short, DefaultWebSocketServerSession>
-    private val idBySession: Map<DefaultWebSocketServerSession, Short>
     private val timeLimit: Short = 3 * 60
     private var time = timeLimit
 
     init {
         val id = AtomicInteger(0)
         sessionById = sessions.associateBy { id.getAndIncrement().toShort() }
-        idBySession = sessionById.entries.associate { (key, value) -> value to key }
     }
 
     /**
@@ -34,12 +39,42 @@ data class Game(
         sessionById.forEach { (id, session) ->
             session.sendPacket(PacketType.StartGame, GameData(id, 2, timeLimit))
         }
+        sendInitialLocation()
+        withContext(Dispatchers.Unconfined) {
+            awaitAll(
+                async { countdown() },
+                *sessionById.map { (id, session) ->
+                    async {
+                        for (frame in session.incoming) {
+                            if (frame is Frame.Binary) {
+                                val packet = frame.readPacket() ?: continue
+                                when (packet.id) {
+                                    PacketType.UpdateSelfLocation.id -> {
+                                        val (x, y, yaw) = packet.toPacket(PacketType.UpdateSelfLocation)?.data as? SelfLocationData ?: continue
+                                        sessions.forEach {
+                                            if (it != session) {
+                                                it.sendPacket(PacketType.UpdateLocation, LocationData(id, x, y, yaw))
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }.toTypedArray()
+            )
+        }
+    }
+
+    /**
+     * 初期位置を送信する
+     */
+    private suspend fun sendInitialLocation() {
         var x = 100
         sessionById.forEach { (id, _) ->
             sessions.sendPacket(PacketType.UpdateLocation, LocationData(id, x, 100, 0))
             x += 200
         }
-        countdown()
     }
 
     /**
