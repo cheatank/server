2e93c4cd05abd61e3b6dd42f608dce093cde2dcc
sya-ri <sya79lua@gmail.com>
2022-01-26 02:48:15 +0900

    feat: suppressCheat のオプションを追加

--- src/main/kotlin/com/github/cheatank/server/Options.kt
+++ src/main/kotlin/com/github/cheatank/server/Options.kt
@@ -8,10 +8,15 @@ import org.slf4j.LoggerFactory
 object Options {
     val timeLimit: Short = System.getProperty("timeLimit")?.toShortOrNull() ?: 180
     val lifeCount: Byte = System.getProperty("lifeCount")?.toByteOrNull() ?: 2
+    val suppressCheat = when (System.getProperty("suppressCheat")?.lowercase()) {
+        "", "true" -> true
+        else -> false
+    }
 
     fun load() {
         val logger = LoggerFactory.getLogger("Options")
         logger.info("timeLimit: $timeLimit")
         logger.info("lifeCount: $lifeCount")
+        logger.info("suppressCheat: $suppressCheat")
     }
 }
